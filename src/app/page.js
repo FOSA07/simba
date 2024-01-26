4362a8c879b68ab895cfb23cfe5b96fa530a76ff
Faleti Oluwaseyi Samuel <faletioluwaseyisam@gmail.com>
2024-01-26 03:57:17 +0100

    update

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,9 @@ const image1 = '/assets/simba_about.png';
 const image2 = '/assets/metamask_icon.png';
 const image3 = '/assets/bnb_icon.png';
 const image4 = '/assets/swap_ico.png';
+const clawIcon = '/assets/claw 2.png';
+const simbaIcon = '/assets/simbaa 1.png';
+
 
 function Navbar() {
 
@@ -31,9 +34,18 @@ function Navbar() {
   return <nav class="navbar navbar-expand-lg navbar-light fixed-top text-light" style={{ background: '#000' }}>
       {isOpen && <MobileNavOverlay isOpen={isOpen} setIsOpen={setIsOpen} />}
       <div class="container-fluid">
-      <a class="navbar-brand text-light" style={{ fontFamily: "Poppins", width: '165px', height: '50px' }} href="#">
-          {/* <img src={image2} style={{width: '100%', height: '100%'}} alt="Logo" /> */}
-      </a>
+      <div class="navbar-brand text-light" style={{ fontFamily: "Poppins", width: '50px', height: '50px', position: 'relative' }} href="#">
+          {/* <img src={simbaIcon} style={{width: '100%', height: '100%'}} alt="Logo" /> */}
+          <Image
+            src={simbaIcon}
+            alt="Background Image"
+            layout="fill"
+            objectFit="fill"
+            className='rounded-4'
+            // width = '111px'
+            // height = '111px'
+          />
+      </div>
 
           <div className='d-none d-lg-block'>
           <div class="navbar-nav ml-auto d-flex ">
@@ -235,13 +247,13 @@ function Navbar() {
 
     }
     return (
-      <div id="about">
+      <div className="mb-5" id="about">
         <div className='d-none d-lg-flex' style={{height: ''}}>
                 <div className='col-12 col-lg-6 d-none d-lg-flex justify-content-center align-items-center'>
                     <img src={image1} alt="Phoenix Token Logo" style={{maxWidth: '80%', maxHeight: '80%'}}/>
                 </div>
 
-                <div className='col-12 col-lg-6 d-none d-lg-flex justify-content-center align-items-center'>
+                <div className='col-12 col-lg-6 d-none d-lg-flex justify-content-center align-items-center '>
                 <div className=' p-5'>
                 <h1 className="" style={{
                             textAlign: "left",
@@ -391,7 +403,7 @@ function Navbar() {
             webkitTextFillColor: "transparent",
             fontFamily: 'Sigmar One'
         }}>HOW TO BUY</h1>
-        <div className="container d-flex justify-content-center align-items-center my-5">
+        <div className="container d-flex justify-content-center align-items-center mb-5">
             <div className="container row col-sm-1 col-md-2 col-lg-2 d-flex rounded-4 p-3" style={{background: '#252525', width: '100%'}}>
               <div className='col-12 col-lg-3 d-lg-none justify-content-center align-items-center p-4 mb-4' style={{position: 'relative', height: '80vw'}}>
               <Image
@@ -482,7 +494,7 @@ function Navbar() {
         </div>
 
         <div className="container d-flex justify-content-center align-items-center my-5">
-            <div className="container row col-sm-1 col-md-2 col-lg-2 d-flex rounded-4 p-3" style={{background: '#252525', width: '80%'}}>
+            <div className="container row col-sm-1 col-md-2 col-lg-2 d-flex rounded-4 p-3" style={{background: '#252525', width: '100%'}}>
               <div className='col-12 col-lg-3 d-lg-none justify-content-center align-items-center p-4 mb-4' style={{position: 'relative', height: '80vw'}}>
               <Image
                     src={image4}
@@ -551,29 +563,228 @@ function Navbar() {
                   fontFamily: 'Sigmar One'
               }}>ROADMAP</h1>
 
-        <div className="container d-flex justify-content-center align-items-center my-5">
+        <div className="container d-flex justify-content-center align-items-center mb-5">
             <div className="container row col-sm-1 col-md-2 col-lg-2 d-flex justify-content-center align-items-center rounded-4 p-3" style={{width: '100%'}}>
               <div className='col-12 col-lg-5 justify-content-center align-items-center p-4 mb-3 rounded-4 bg-white' style={{height: '300px'}}>
-                <p>Website development</p>
-                <p>Deploy Smart Contract (BNB)</p>
-                <p>Social Media Launch</p>
-                <p>Community AMA</p>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Website development</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Deploy Smart Contract (BNB)</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Social Media Launch</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Community AMA</p></div>
+                </div>
+                
               </div>
 
               <div className="d-none d-lg-block col-1"></div>
               <div className='col-12 col-lg-5 justify-content-center align-items-center p-4 mb-3 rounded-4 bg-white' style={{height: '300px'}}>
-                <p>Marketting Campaign</p>
-                <p>Coingecko Listing</p>
-                <p>Stacking</p>
-                <p>CMS Listing</p>
-                <p>Trending on all Platforms</p>
-                <p>Massive Marketting Campaign</p>
+
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Marketting Campaign</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Coingecko Listing</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Stacking</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>CMS Listing</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Trending on all Platforms</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Massive Marketting Campaign</p></div>
+                </div>
+                
               </div>
 
               <div className='col-12 col-lg-5 justify-content-center align-items-center p-4 mb-3 rounded-4 bg-white' style={{height: '300px'}}>
-                <p>Official White Paper Launch</p>
-                <p>Listing on CEX Exchange</p>
-                <p>New RoadMap Release</p>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Official White Paper Launch</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>Listing on CEX Exchange</p></div>
+                </div>
+                <div className="d-flex">
+                <div className="" style={{width: '20px', height: '20px', position: 'relative'}}>
+                  
+                    <Image
+                      src={clawIcon}
+                      alt="Background Image"
+                      layout="fill"
+                      objectFit="fill"
+                      className='rounded-4'
+                      
+                    />
+
+                    
+                </div>
+                <div className="px-2"><p>New RoadMap Release</p></div>
+                </div>
+                
               </div>
           </div>
         </div>
@@ -585,7 +796,7 @@ function Navbar() {
   function Tokenomics() {
 
     return(
-      <div id='tokenomics py-5'>
+      <div className="container" id='tokenomics'>
         <h1 className="" style={{
                   textAlign: "center",
                   // textShadow: "0px 18px 7px rgba(0, 0, 0, 0.70)",
@@ -726,7 +937,17 @@ function Navbar() {
                 <p className="text-center text-light" style={{  }}>Our community of Immortals with the love for humanity grow stronger with each passing day. Please follow our social platforms to get the most up-to-date, accurate PHT information.</p>
 
                 <div className="p-3 d-flex justify-content-center">
-                    <div className="rounded-4 d-flex justify-content-space-around" style={{ width: '160px' }}>
+                    <div className="rounded-4 d-flex justify-content-around" style={{ width: '160px' }}>
+                      <div style={{}}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 78 80" fill="none">
+                          <path d="M39 0C17.4556 0 0 17.9032 0 40C0 62.0968 17.4556 80 39 80C60.5444 80 78 62.0968 78 40C78 17.9032 60.5444 0 39 0ZM58.154 27.4032L51.7536 58.3387C51.2819 60.5323 50.0081 61.0645 48.231 60.0323L38.481 52.6613L33.779 57.3064C33.2601 57.8387 32.8198 58.2903 31.8133 58.2903L32.5052 48.1129L50.5742 31.371C51.3605 30.6613 50.4012 30.2581 49.3633 30.9677L27.0327 45.3871L17.4085 42.3064C15.3169 41.629 15.2698 40.1613 17.8488 39.129L55.4492 24.2581C57.1948 23.6129 58.7202 24.6935 58.154 27.4032Z" fill="white"/>
+                        </svg>
+                      </div>
+                      <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 80 80" fill="none">
+                          <path d="M71.4286 0H8.57143C3.83929 0 0 3.83929 0 8.57143V71.4286C0 76.1607 3.83929 80 8.57143 80H71.4286C76.1607 80 80 76.1607 80 71.4286V8.57143C80 3.83929 76.1607 0 71.4286 0ZM62.6964 28.3571C62.7321 28.8571 62.7321 29.375 62.7321 29.875C62.7321 45.3571 50.9464 63.1964 29.4107 63.1964C22.7679 63.1964 16.6071 61.2679 11.4286 57.9464C12.375 58.0536 13.2857 58.0893 14.25 58.0893C19.7321 58.0893 24.7679 56.2321 28.7857 53.0893C23.6429 52.9821 19.3214 49.6071 17.8393 44.9643C19.6429 45.2321 21.2679 45.2321 23.125 44.75C17.7679 43.6607 13.75 38.9464 13.75 33.25V33.1071C15.3036 33.9821 17.125 34.5179 19.0357 34.5893C17.4293 33.5208 16.1125 32.0712 15.2026 30.3699C14.2928 28.6686 13.8183 26.7686 13.8214 24.8393C13.8214 22.6607 14.3929 20.6607 15.4107 18.9286C21.1786 26.0357 29.8393 30.6786 39.5536 31.1786C37.8929 23.2321 43.8393 16.7857 50.9821 16.7857C54.3571 16.7857 57.3929 18.1964 59.5357 20.4821C62.1786 19.9821 64.7143 19 66.9643 17.6607C66.0893 20.375 64.25 22.6607 61.8214 24.1071C64.1786 23.8571 66.4643 23.1964 68.5714 22.2857C66.9821 24.625 64.9821 26.6964 62.6964 28.3571Z" fill="white"/>
+                        </svg>
+                      </div>
                         {/* <i style={{ color: 'white', margin: '0 auto', fontSize: '35px', cursor: 'pointer' }} onClick={handleTelegramClick} class="bi bi-telegram"></i> */}
                         {/* <i style={{ color: 'white', margin: '0 auto', fontSize: '35px', cursor: 'pointer' }} onClick={handleTwitterClick} class="bi bi-twitter"></i> */}
                     </div>
